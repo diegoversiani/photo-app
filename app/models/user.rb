3cd713c9cf0f4b95ffbb2ade9dfcaa384d91a3f2
diegoversiani <diegoversiani@gmail.com>
2016-09-12 17:03:37 -0300

    Setup email functionality for signup and add navigation partial.

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,8 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable,
          :validatable, :confirmable
+
+  def full_name
+    email
+  end
 end
